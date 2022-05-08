04f08d2bce15406f7526a4cb87cdd6f8aa22d95e
shevvss <marin.shevchik@gmail.com>
2022-05-09 01:56:11 +0400

    add homework №2

---
+++ homework2/index.js
@@ -0,0 +1,56 @@
+function makeCopyObj(obj) {
+  const clone = {};
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      clone[key] = makeCopyObj(obj[key]);
+    } else {
+      clone[key] = obj[key];
+    }
+  }
+  return clone;
+}
+
+function selectFromInterval(arr, val1, val2) {
+  const isNotNumber = (element) => typeof element != 'number';
+
+  if (
+    !Array.isArray(arr) ||
+    arr.some(isNotNumber) ||
+    !isFinite(val1) ||
+    !isFinite(val2)
+  ) {
+    throw new Error('incorrect data');
+  }
+
+  return arr.filter((item) => {
+    if (val1 < val2) {
+      return val1 <= item && item <= val2;
+    } else {
+      return val2 <= item && item <= val1;
+    }
+  });
+}
+
+const myIterable = {
+  from: 5,
+  to: 4,
+  [Symbol.iterator]() {
+    if (!isFinite(this.from) || !isFinite(this.to) || this.from > this.to) {
+      throw new Error('incorect data');
+    }
+    return {
+      current: this.from,
+      last: this.to,
+      next() {
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        }
+        return { done: true };
+      },
+    };
+  },
+};
+
+for (let item of myIterable) {
+  console.log(item);
+}
